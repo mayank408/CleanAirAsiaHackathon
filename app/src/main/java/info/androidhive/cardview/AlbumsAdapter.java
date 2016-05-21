323aa8d7b56d1ba01350650aedea9a2586d11bc0
Lincoln <Lincoln@apples-MacBook-Air-2.local>
2016-05-21 14:19:49 +0530

    adding project

---
+++ app/src/main/java/info/androidhive/cardview/AlbumsAdapter.java
@@ -0,0 +1,54 @@
+package info.androidhive.cardview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Lincoln on 18/05/16.
+ */
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHolder> {
+
+    private List<Album> albumList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        }
+    }
+
+
+    public AlbumsAdapter(List<Album> albumList) {
+        this.albumList = albumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.album_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Album album = albumList.get(position);
+        holder.title.setText(album.getName());
+        holder.thumbnail.setImageResource(album.getThumbnail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+}
